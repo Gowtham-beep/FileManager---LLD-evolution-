0d233503829c71f72b0ad30c479484e3adfab619
Gowtham-beep <gn86923@gmail.com>
2026-01-30 15:35:25 +0530

    feat: update App.java to demonstrate file manager functionality
    Replace "Hello World!" print with file manager initialization, event notifier setup, and test file upload to showcase core features.

--- src/main/java/com/fileManager/App.java
+++ src/main/java/com/fileManager/App.java
@@ -1,10 +1,18 @@
 package com.fileManager;
 
-/**
- * Hello world!
- */
+import com.fileManager.FileManager.FileManager;
+import com.fileManager.Notification.EventNotifier;
+import com.fileManager.Notification.Observer.UpdateFileObserver;
+import com.fileManager.fileIO.LocalFileIO;
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("FILE MANAGER!");
+        FileManager fileManager = new FileManager(new LocalFileIO());
+        EventNotifier.getInstance().addObserver(new UpdateFileObserver());
+
+        String data = "TESTING THE FILE MANAGER";
+        fileManager.upload("/home/wbtserver/IdeaProjects/fileManager/test.txt",data.getBytes());
     }
 }
